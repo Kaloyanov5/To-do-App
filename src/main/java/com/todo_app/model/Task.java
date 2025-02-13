4ad40c8f7a83e521347dfd13e6e4d47b9128a922
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-02-13 15:34:26 +0200

    Renamed Task model file

---
+++ src/main/java/com/todo_app/model/Task.java
@@ -0,0 +1,45 @@
+package com.todo_app.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private Boolean done;
+
+    public Task(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
