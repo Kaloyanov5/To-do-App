99ccec40601b8d55196addb5bd4956c987ae92b3
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-02-13 16:20:32 +0200

    Added request objects for POST requests

---
+++ src/main/java/com/todo_app/model/TaskRequest.java
@@ -0,0 +1,13 @@
+package com.todo_app.model;
+
+public class TaskRequest {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/com/todo_app/model/UpdateRequest.java
@@ -0,0 +1,13 @@
+package com.todo_app.model;
+
+public class UpdateRequest {
+    private boolean done;
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
